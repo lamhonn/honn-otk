4a411addeeabb9be084fbef4095489e496ac9db6
lamhonn <36136989+lamhonn@users.noreply.github.com>
2021-03-17 16:46:01 +0200

    Add files via upload

---
+++ src/test/java/fi/vy/lamh/AppTest.java
@@ -0,0 +1,19 @@
+package fi.vy.lamh;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest {
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue() {
+        assertTrue(true);
+    }
+}
