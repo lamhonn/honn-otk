cee049ad7d3190dfb7db516a1551b3e5e33a3788
lamhonn <lamhonn@gmail.com>
2021-03-17 22:20:35 +0200

    lost count on commits

--- src/main/java/fi/vy/lamh/App.java
+++ src/main/java/fi/vy/lamh/App.java
@@ -1,12 +1,32 @@
 package fi.vy.lamh;
 
-/**
- * Hello world!
- *
- */
+import java.text.DecimalFormat;
+
 public class App {
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        System.out.println("2 plus 3 on" + add(2,3));
+        System.out.println("1 is divided by 0 is on " + divide(1,0));
     }
 
+    public static int add(int a, int b) {
+        return a+b;
+    }
+
+    /*Function get two doubles as arguments and
+    *  return the division of them in two decimals. 
+    * if denominator is zero return NaN*/
+
+    public static double divide(double a, double b) {
+        DecimalFormat df = new DecimalFormat("#.##");
+        return Double.valueOf(df.format(a / b).replace(",", ".")).doubleValue();
+    }
+
+    public static String returnTwoChars(String input) {
+        return input.substring(0,1);
+    }
+
+    public static Double div(double a, double b) {
+        return a/b;
+    }
 }
--- src/test/java/fi/vy/lamh/AppTest.java
+++ src/test/java/fi/vy/lamh/AppTest.java
@@ -5,15 +5,47 @@ import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
 
+import jdk.jfr.Timestamp;
+
 /**
  * Unit test for simple App.
  */
 public class AppTest {
-    /**
-     * Rigorous Test :-)
-     */
+
     @Test
     public void shouldAnswerWithTrue() {
         assertTrue(true);
+    }
+
+    @Test
+    public void testAdd() {
+        assertEquals(10, add(6,4));
+        assertEquals(0, add(0,0));
+        assertEquals(0, add(-6,6));
+    }
+
+    @Test
+    public void testDivide() {
+        assertEquals(1.33, divide(4,3));
+        assertEquals(0, divide(0,1));
+        assertEquals(NaN, divide(1,0));
+    }
+
+    @Test
+    public void testReturnTwoChars() {
+        assertEquals("qw", returnTwoChars("qwerty"));
+        assertEquals("VA", returnTwoChars("VA"));
+        assertEquals("V", returnTwoChars("V"));
+        assertEquals("", returnTwoChars(""));
+        assertEquals(null, returnTwoChars(null));
+    }
+
+    @Test
+    public void testDiv() {
+        assertEquals(1.33, div(new Double(4), new Double(3)));
+        assertEquals(33, div(new Double(99), new Double(3)));
+        assertEquals(null, div(null, new Double(3)));
+        assertEquals(null, div(new Double(3), null));
+        assertEquals(null, div(new Double(3), new Double(0)));
     }
 }
